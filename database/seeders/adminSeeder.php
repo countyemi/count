c601bf44d975b14ada1643ac8543d3862d52c903
countyemi <adeyemiadeleye@rocketmail.com>
2024-04-10 23:04:48 -0400

    Update adminSeeder.php

--- database/seeders/adminSeeder.php
+++ database/seeders/adminSeeder.php
@@ -3,20 +3,23 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class adminSeeder extends Seeder
+class AdminSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        User::create([
-            'name' => 'Admin',
-            'email' => 'admin@gmail.com',
-            'password' => bcrypt('123456'),
-        ]);
+        // Check if the user already exists
+        if (!User::where('email', 'admin@gmail.com')->exists()) {
+            // Create the user only if it doesn't already exist
+            User::create([
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('123456'),
+            ]);
+        }
     }
 }
